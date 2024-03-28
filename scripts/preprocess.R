3770c706f74c7b406d22b9ca884ba9aa10988464
shruthibandyadka <shruthibhat93@gmail.com>
2024-03-28 15:46:20 -0400

    full QC workflow

--- scripts/preprocess.R
+++ scripts/preprocess.R
@@ -32,6 +32,57 @@ inputdir <- paste0(basedir,"cellranger/")
 outputdir <- paste0(basedir,"preprocess/")
 dir.create(outputdir,showWarnings = FALSE)
 
+
+## Load raw w1118_42d counts
+
+w1118_42d <- importCellRangerV3(
+  cellRangerDirs = inputdir,
+  sampleDirs = "w1118_42D",
+  sampleNames = "w1118_42D",
+  dataType = "filtered")
+
+
+w1118_42d_samples <- colData(w1118_42d)$sample
+
+## Run SCTK-QC on w1118_42d cells
+
+w1118_42d <- runCellQC(w1118_42d, 
+                       algorithms = c("QCMetrics","doubletFinder", "decontX","cxds", "bcds", "cxds_bcds_hybrid"), 
+                       sample = w1118_42d_samples)
+
+## Convert SCE object to Seurat for downstream analysis
+w1118_42d <- setRowNames(w1118_42d,"feature_name") ## use gene symbols instead of flybase id
+w1118_42d_seurat <- as.Seurat(w1118_42d,data = NULL)
+w1118_42d_seurat[['decontXcounts']] <- CreateAssayObject(counts=assays(w1118_42d)$decontXcounts)
+w1118_42d_seurat$nCount_RNA <- as.numeric(w1118_42d_seurat$nCount_originalexp)
+w1118_42d_seurat$nFeature_RNA <-as.numeric( w1118_42d_seurat$nFeature_originalexp)
+
+## Load raw drprnull_42d counts
+drprnull_42d <- importCellRangerV3(
+  cellRangerDirs = inputdir,
+  sampleDirs = "drprnull_42D",
+  sampleNames = "drprnull_42D",
+  dataType = "filtered")
+
+drprnull_42d_samples <- colData(drprnull_42d)$sample
+
+
+## Run SCTK-QC on drprnull_42d cells
+drprnull_42d <- runCellQC(drprnull_42d, 
+                          algorithms = c("QCMetrics","doubletFinder", "decontX","cxds", "bcds", "cxds_bcds_hybrid"), 
+                          sample = drprnull_42d_samples)
+
+## Convert SCE object to Seurat for downstream analysis
+drprnull_42d <- setRowNames(drprnull_42d,"feature_name") ## use gene symbols instead of flybase id
+drprnull_42d_seurat <- as.Seurat(drprnull_42d,data = NULL)
+drprnull_42d_seurat[['decontXcounts']] <- CreateAssayObject(counts=assays(drprnull_42d)$decontXcounts)
+drprnull_42d_seurat$nCount_RNA <- as.numeric(drprnull_42d_seurat$nCount_originalexp)
+drprnull_42d_seurat$nFeature_RNA <- as.numeric(drprnull_42d_seurat$nFeature_originalexp)
+
+## Combine w1118_42d and drprnull_42d seurat objects into a list for easy analysis
+data_list <- list("w1118_42d" = w1118_42d_seurat,"drprnull_42d" = drprnull_42d_seurat)
+
+## Filter cells based on QC metrics 
 ## Function definitions
 
 mad_outlier <- function(sobj, metric, nmads){
@@ -58,6 +109,13 @@ read_raw_sobj <- function(sample_id){
 }
 
 flag_outliers <- function(sobj){
+  
+  #add QC metrics
+  sobj$log1p_total_counts <- log1p(sobj@meta.data$nCount_RNA)
+  sobj$log1p_n_genes_by_counts <- log1p(sobj@meta.data$nFeature_RNA)
+  sobj[["percent.mt"]] <- PercentageFeatureSet(sobj, pattern = "^mt:")
+  
+  
   ncount_outlier <- mad_outlier(sobj, 'log1p_total_counts', 5) 
   names(ncount_outlier) <- colnames(sobj)
   sobj <- AddMetaData(object = sobj,metadata = ncount_outlier,col.name = 'nCount_outlier')
@@ -104,15 +162,15 @@ plot_features <- function(sobjmetadata){
 }
 
 plot_outliers <- function(sobjmetadata){
-  ncount_outlier_plot <- ggplot(data = sobjmetadata, aes(x=sample_id,y=log1p_n_genes_by_counts,color=nFeature_outlier))+
+  ncount_outlier_plot <- ggplot(data = sobjmetadata, aes(x=sample,y=log1p_n_genes_by_counts,color=nFeature_outlier))+
     #geom_violin(alpha=0.5)+
     geom_jitter(alpha=0.5)+
     theme_classic(base_size = 16)
-  nfeature_outlier_plot <- ggplot(data = sobjmetadata, aes(x=sample_id,y=log1p_total_counts,color=nCount_outlier))+
+  nfeature_outlier_plot <- ggplot(data = sobjmetadata, aes(x=sample,y=log1p_total_counts,color=nCount_outlier))+
     #geom_violin(alpha=0.5)+
     geom_jitter(alpha=0.5)+
     theme_classic(base_size = 16)
-  mt_outlier_plot <- ggplot(data = sobjmetadata, aes(x=sample_id,y=percent.mt,color=mt_outlier))+
+  mt_outlier_plot <- ggplot(data = sobjmetadata, aes(x=sample,y=percent.mt,color=mt_outlier))+
     #geom_violin(alpha=0.5)+
     geom_jitter(alpha=0.5)+
     theme_classic(base_size = 16)
@@ -129,120 +187,37 @@ filter_by_counts <- function(sobj){
 
 
 ## Read raw counts and identify outliers
-samples <- c('w1118_42D', 'drprnull_42D')
-data_list <- sapply(samples, read_raw_sobj)
+samples <- c('w1118_42d', 'drprnull_42d')
+#data_list <- sapply(samples, read_raw_sobj)
 data_list <- sapply(data_list, flag_outliers)
 
 
 ## Plot features before filtering
-plot_features(data_list[1]$w1118_42D@meta.data)
-plot_features(data_list[2]$drprnull_42D@meta.data)
+plot_features(data_list[1]$w1118_42d@meta.data)
+plot_features(data_list[2]$drprnull_42d@meta.data)
 
 
 ## Plot outliers before filtering
-plot_outliers(data_list[1]$w1118_42D@meta.data)
-plot_outliers(data_list[2]$drprnull_42D@meta.data)
+plot_outliers(data_list[1]$w1118_42d@meta.data)
+plot_outliers(data_list[2]$drprnull_42d@meta.data)
+
+### Removing percent.mt > 0.5% cells in drpr sample removes more than half the samples. Finding a more relaxed threshold
+drpr_percentmt_filter <- as.data.frame(data_list[2]$drprnull_42d@meta.data) %>%
+  filter(percent.mt <5)
+print(dim(drpr_percentmt_filter))
+ggplot(drpr_percentmt_filter, aes(x=percent.mt))+
+  geom_histogram()+
+  theme_classic(base_size = 16)
 
 ## Filter low quality cells
 data_list <- sapply(data_list, filter_by_counts)
 
 ## Plot features after filtering
-plot_features(data_list[1]$w1118_42D@meta.data)
-plot_features(data_list[2]$drprnull_42D@meta.data)
+plot_features(data_list[1]$w1118_42d@meta.data)
+plot_features(data_list[2]$drprnull_42d@meta.data)
 
-## Normalization
-data_list <- sapply(data_list, SCTransform, vars.to.regress ="percent.mt")
-
-
-ggplot(data=data_list[1]$w1118_42D@meta.data,aes(x=log10(nCount_SCT)))+
-  geom_histogram()+
-  theme_classic(base_size = 16)
-
-ggplot(data=data_list[2]$drprnull_42D@meta.data,aes(x=nCount_SCT))+
-  geom_histogram()+
-  theme_classic(base_size = 16)
-
-ggplot(data=data_list[1]$w1118_42D@meta.data,aes(x=nFeature_SCT))+
-  geom_histogram()+
-  theme_classic(base_size = 16)
-
-ggplot(data=data_list[2]$drprnull_42D@meta.data,aes(x=nFeature_SCT))+
-  geom_histogram()+
-  theme_classic(base_size = 16)
-
-ggplot(as.data.frame(data_list[1]$w1118_42D@meta.data), aes(x=nCount_SCT,y=nFeature_SCT, color=percent.mt))+
-  geom_point()+
-  ggtitle("W1118_42D")+
-  scale_color_viridis_c(option = "magma")+
-  theme_classic(base_size = 16)
-
-ggplot(as.data.frame(data_list[2]$drprnull_42D@meta.data), aes(x=nCount_SCT,y=nFeature_SCT, color=percent.mt))+
-  geom_point()+
-  ggtitle("drprnull_42D")+
-  scale_color_viridis_c(option = "magma")+
-  theme_classic(base_size = 16)
-
-
-## Clustering 
-
-run_clustering <- function(sobj){
-  sobj <- RunPCA(sobj, verbose = FALSE)
-  sobj <- RunUMAP(sobj, dims = 1:30, verbose = FALSE)
-  
-  sobj <- FindNeighbors(sobj, dims = 1:30, verbose = FALSE)
-  
-  sobj <- FindClusters(sobj, verbose = FALSE, resolution=0.02)
-  sobj <- FindClusters(sobj, verbose = FALSE, resolution=0.2)
-  sobj <- FindClusters(sobj, verbose = FALSE, resolution=0.8)
-  sobj <- FindClusters(sobj, verbose = FALSE, resolution=1)
-  sobj <- FindClusters(sobj, verbose = FALSE, resolution=1.5)
-  sobj <- FindClusters(sobj, verbose = FALSE, resolution=2.5)
-  sobj <- FindClusters(sobj, verbose = FALSE, resolution=3.5)
-  sobj <- FindClusters(sobj, verbose = FALSE, resolution=4.5)
-  sobj <- FindClusters(sobj, verbose = FALSE, resolution=8)
-  sobj <- FindClusters(sobj, verbose = FALSE, resolution=12)
-  return(sobj)
-}
-
-data_list <- sapply(data_list, run_clustering)
-
-cluster_res <- c("SCT_snn_res.0.02","SCT_snn_res.0.2","SCT_snn_res.0.8","SCT_snn_res.1","SCT_snn_res.1.5","SCT_snn_res.2.5",
-                 "SCT_snn_res.3.5","SCT_snn_res.4.5","SCT_snn_res.8","SCT_snn_res.12")
-
-markers <- c("elav","lncRNA:noe","VAChT","VGlut","Gad1","Vmat","SerT","Tdc2","ple", # neurons
-             "repo","lncRNA:CR34335","alrm","wrapper","Indy","moody",#glia 
-             "ninaC",	"trp",	"trpl", #photoreceptors
-             "Hml", #hemocytes
-             "ppl",#fatbody
-             "drpr")
-
-
-for(cr in cluster_res){
-  p1 <- DimPlot(data_list[1]$w1118_42D,group.by = cr) &
-    theme_classic(base_size = 16)
-  
-  ggsave(p1,filename=paste0(outputdir,"figures/","w1118_42d",cr,"_dimplot.pdf"),width=10,height=10)
-  
-  p2 <- DimPlot(data_list[2]$drprnull_42D,group.by = cr)&
-    theme_classic(base_size = 16)
-  
-  ggsave(p2,filename=paste0(outputdir,"figures/","drprnull_42d",cr,"_dimplot.pdf"),width=10,height=10)
-  
-  p3 <- DotPlot(data_list[1]$w1118_42D, features = markers, group.by = cr)&
-    scale_color_viridis_c(option = "magma",direction = -1)&
-    theme_classic(base_size = 16) + RotatedAxis()
-  
-  ggsave(p3,filename=paste0(outputdir,"figures/","w1118_42d",cr,"_dotplot.pdf"), width=16,height=10)
-  
-  p4 <- DotPlot(data_list[2]$drprnull_42D, features = markers, group.by = cr)&
-    scale_color_viridis_c(option = "magma",direction = -1)&
-    theme_classic(base_size = 16)+ RotatedAxis()
-  
-  ggsave(p4,filename=paste0(outputdir,"figures/","drprnull_42d",cr,"_dotplot.pdf"), width=16,height=10)
-  
-}
-
-
+data_list[1]$w1118_42d ##9033 cells x 16507 genes 
+data_list[2]$drprnull_42d ##6423 cells x 16507 genes 
 
 ## Ambient RNA correction using SoupX
 
@@ -261,8 +236,8 @@ get_soup_groups <- function(sobj){
 }
 
 add_soup_groups <- function(sobj){
-  #sobj <- get_soup_groups(sobj)
-  sobj$soup_group <- sobj@meta.data[['SCT_snn_res.0.8']] ## Use Clusters from SCT normalized assay
+  sobj <- get_soup_groups(sobj)
+  sobj$soup_group <- sobj@meta.data[['seurat_clusters']]
   return(sobj)
 }
 
@@ -270,16 +245,16 @@ data_list <- sapply(data_list, add_soup_groups)
 
 make_soup <- function(sobj){
   inputdir <- "/projectnb/mccall/sbandyadka/drpr42d_snrnaseq/analysis/cellranger/"
-  sample_id <- as.character(sobj$sample_id[1]) 
+  sample_id <- as.character(sobj$sample[1]) 
   path <- paste0(inputdir,sample_id, "/outs/raw_feature_bc_matrix/")
   raw <- Read10X(data.dir = path)
   
-  sc = SoupChannel(raw,sobj@assays$RNA@counts)
+  sc = SoupChannel(raw,sobj@assays$originalexp$counts)
   sc = setClusters(sc,sobj$soup_group)
   sc = autoEstCont(sc, doPlot=FALSE)
   out = adjustCounts(sc, roundToInt = TRUE)
   
-
+  
   
   #sobj[["original.counts"]] <- CreateAssayObject(counts = sobj@assays$RNA@counts)
   #sobj@assays$RNA@counts <- out
@@ -292,95 +267,22 @@ make_soup <- function(sobj){
 
 data_list <- sapply(data_list, make_soup)
 
+
 ###  Check SoupX correction - % of RNA removed
 
-sum(data_list[1]$w1118_42D@assays$RNA@counts)
-sum(data_list[1]$w1118_42D@assays$soupXcounts@counts)/sum(data_list[1]$w1118_42D@assays$RNA@counts)*100
+sum(data_list[1]$w1118_42d@assays$originalexp$counts)
+sum(data_list[1]$w1118_42d@assays$soupXcounts$counts)/sum(data_list[1]$w1118_42d@assays$originalexp@counts)*100
 
-sum(data_list[2]$drprnull_42D@assays$RNA@counts)
-sum(data_list[2]$drprnull_42D@assays$soupXcounts@counts)/sum(data_list[2]$drprnull_42D@assays$RNA@counts)*100
-
-## Convert Seurat objects to SingleCellExperiment objects
-data_list_sce <- sapply(data_list, as.SingleCellExperiment)
+sum(data_list[2]$drprnull_42d@assays$originalexp$counts)
+sum(data_list[2]$drprnull_42d@assays$soupXcounts@counts)/sum(data_list[2]$drprnull_42d@assays$originalexp@counts)*100
 
 
+## Normalization
+data_list <- sapply(data_list, SCTransform, vars.to.regress ="percent.mt",assay="originalexp", new.assay.name="SCT")
+data_list <- sapply(data_list, SCTransform, vars.to.regress ="percent.mt",assay="soupXcounts", new.assay.name="soupX_SCT")
+data_list <- sapply(data_list, SCTransform, vars.to.regress ="percent.mt",assay="decontXcounts", new.assay.name="decontX_SCT")
 
-DefaultAssay(data_list[2]$drprnull_42D) <- "original.counts"
-DefaultAssay(data_list[1]$w1118_42D) <- "original.counts"
-FeaturePlot(data_list[2]$drprnull_42D, features=c("elav","repo","drpr"))
-FeaturePlot(data_list[1]$w1118_42D, features=c("elav","repo","drpr"))
-
-DefaultAssay(data_list[2]$drprnull_42D) <- "RNA"
-DefaultAssay(data_list[1]$w1118_42D) <- "RNA"
-
-FeaturePlot(data_list[1]$w1118_42D, features=c("repo","drpr"), blend = TRUE)
-FeaturePlot(data_list[2]$drprnull_42D, features=c("repo","drpr"), blend = TRUE)
-
-
-
-## Load raw w1118_42d counts
-
-
-w1118_42d <- importCellRangerV3(
-  cellRangerDirs = inputdir,
-  sampleDirs = "w1118_42D",
-  sampleNames = "w1118_42D",
-  dataType = "filtered")
-
-
-w1118_42d_samples <- colData(w1118_42d)$sample
-
-## Run SCTK-QC on w1118_42d cells
-
-w1118_42d <- runCellQC(w1118_42d, 
-                       algorithms = c("QCMetrics","doubletFinder", "decontX","cxds", "bcds", "cxds_bcds_hybrid"), 
-                       sample = w1118_42d_samples)
-
-
-drprnull_42d <- importCellRangerV3(
-    cellRangerDirs = inputdir,
-    sampleDirs = "drprnull_42D",
-    sampleNames = "drprnull_42D",
-    dataType = "filtered")
-
-
-drprnull_42d_samples <- colData(drprnull_42d)$sample
-
-## Normalize and log1p transform counts
-
-drprnull_42d <- runNormalization(
-  inSCE = drprnull_42d,
-  useAssay = "counts",
-  outAssayName = "logcounts",
-  normalizationMethod = "LogNormalize",
-  scale = FALSE,
-  seuratScaleFactor = 10000,
-  transformation = "log1p",
-  pseudocountsBeforeNorm = NULL,
-  pseudocountsBeforeTransform = NULL,
-  trim = NULL,
-  verbose = TRUE
-  )
-
-
-## Run SCTK-QC
-drprnull_42d <- runCellQC(drprnull_42d, 
-                 algorithms = c("QCMetrics","doubletFinder", "decontX","cxds", "bcds", "cxds_bcds_hybrid"), 
-                 sample = drprnull_42d_samples)
-
-drprnull_42d <- runCellQC(drprnull_42d, 
-                          algorithms = c("soupX"), 
-                          sample = drprnull_42d_samples)
-
-## Run SoupX
-
-
-drprnull_42d_soupxc <- Seurat::Read10X(file.path(inputdir, "drprnull_42D","outs","raw_feature_bc_matrix"))
-drprnull_42d_soupxc <- autoEstCont(drprnull_42d_soupxc)
-out = adjustCounts(sc)
-
-
-
+saveRDS(data_list, file=paste0(outputdir,"data_list.RDS"))
 
 writeH5AD(
   drprnull_42d,
@@ -402,7 +304,7 @@ writeH5AD(
 
 
 
-
+sessionInfo()
 
 
 
